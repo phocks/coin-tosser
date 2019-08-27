d7fda94099def648c592a611edd11808bc237061
Joshua Byrd <phocks@gmail.com>
2019-08-27 13:08:35 +1000

    Calculate average of rounds

--- src/main.rs
+++ src/main.rs
@@ -2,38 +2,46 @@ use rand::prelude::*;
 
 fn main() {
   let how_many_tosses = 10;
+  let number_of_rounds = 100;
 
-  let mut iteration = 0;
-  let mut done = false;
+  let mut average = 0;
 
-  while !done {
-    println!("Iteration: {}", iteration);
+  for x in 0..number_of_rounds {
+    let mut iteration = 0;
+    let mut done = false;
 
-    let mut heads = 0;
-    let mut tails = 0;
+    while !done {
+      println!("Iteration: {}", iteration);
 
-    for x in 0..how_many_tosses {
-      if random() {
-        // generates a boolean
-        // println!("Heads!");
-        heads = heads + 1;
-      } else {
-        // println!("Tails!");
-        tails = tails + 1;
+      let mut heads = 0;
+      let mut tails = 0;
+
+      for x in 0..how_many_tosses {
+        if random() {
+          // generates a boolean
+          // println!("Heads!");
+          heads += 1;
+        } else {
+          // println!("Tails!");
+          tails += 1;
+        }
       }
+
+      println!("{} heads", heads);
+      println!("{} tails", tails);
+
+      if heads == 0 {
+        done = true;
+      }
+      if tails == 0 {
+        done = true;
+      }
+      iteration += 1;
     }
 
-    println!("{} heads", heads);
-    println!("{} tails", tails);
-
-    if heads == 0 {
-      done = true;
-    }
-    if tails == 0 {
-      done = true;
-    }
-    iteration += 1;
+    println!("Took {} iterations to get zero result!", iteration);
+    average += iteration;
   }
 
-  println!("Took {} iterations to get zero result!", iteration);
+  println!("Average: {}", average / number_of_rounds);
 }
