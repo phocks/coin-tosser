0443d3f3e15e497143d5d7e57ef069dbf1a3da25
Joshua Byrd <phocks@gmail.com>
2019-08-27 11:48:12 +1000

    Working???

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,39 @@
 use rand::prelude::*;
 
 fn main() {
-  // We can use random() immediately. It can produce values of many common types:
-  // let x: u8 = random();
-  // println!("{}", x);
+  let how_many_tosses = 15;
 
-  let mut heads = 0;
-  let mut tails = 0;
+  let mut iteration = 0;
+  let mut done = false;
 
-  for x in 0..10 {
-    if random() {
-      // generates a boolean
-      println!("Heads!");
-      heads = heads + 1;
-    } else {
-      println!("Tails!");
-      tails = tails + 1;
+  while !done {
+    println!("Iteration: {}", iteration);
+
+    let mut heads = 0;
+    let mut tails = 0;
+
+    for x in 0..how_many_tosses {
+      if random() {
+        // generates a boolean
+        // println!("Heads!");
+        heads = heads + 1;
+      } else {
+        // println!("Tails!");
+        tails = tails + 1;
+      }
     }
+
+    println!("{} heads", heads);
+    println!("{} tails", tails);
+
+    if heads == 0 {
+      done = true;
+    }
+    if tails == 0 {
+      done = true;
+    }
+    iteration += 1;
   }
 
-  println!("{} heads", heads);
-  println!("{} tails", tails);
+  println!("Took {} iterations to get zero result!", iteration);
 }
